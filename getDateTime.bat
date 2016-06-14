160a460cf637d59734cf7088c61b04f760a2a5b1
jason3e7 <jason3e7@gmail.com>
2016-06-14 14:19:27 +0800

    Add fileList output to file

--- getDateTime.bat
+++ getDateTime.bat
@@ -1,5 +1,7 @@
 @echo off
-rem fileList.bat
+rem getDateTime.bat
+rem date /t
+rem echo %TIME%
 For /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set fDate=%%a%%b%%c)
 For /f "tokens=1-3 delims=/:" %%a in ("%TIME%") do (set fTime=%%a%%b)
-dir /S /B /ON > %fDate%%fTime%.txt
+echo %fDate%%fTime%
